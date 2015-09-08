43ef7dfbde61bf969c78c700eed44999aad0d314
Johnathan C. Maudlin <jcmdln@users.noreply.github.com>
2015-09-07 22:24:19 -0400

    Add curl/wget detection

--- hblk.sh
+++ hblk.sh
@@ -6,6 +6,7 @@
 # unwanted web traffic via host files.
 ############################################################
 
+
 name="hblk"
 NAME="Hblk"
 version="0.0.1"
@@ -15,6 +16,20 @@ version="0.0.1"
 yoyo="http://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts"
 mvps="http://winhelp2002.mvps.org/hosts.txt"
 
+
+# Curl or WGET?
+if [ /bin/curl -eq 1 ]; then
+    dler="curl"
+else
+    if [ /bin/wget -eq 1 ]; then
+	dler="wget"
+    else
+	echo "Install curl, wget, or edit this script and choose your own downloader"
+    fi
+fi
+
 # Lists need to be downloaded into a temporary folder
+$dler
+
 ## Combine lists
 ### Check for duplicates
