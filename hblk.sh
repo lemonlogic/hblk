9280d21e0d90ce4eb6b7e0fe9a835733f1f603d8
jnthnlmn@gmail.com <jnthnlmn@gmail.com>
2015-09-07 20:24:23 -0400

    Initialization.

---
+++ hblk.sh
@@ -0,0 +1,13 @@
+#!bin/sh
+############################################################
+#  Hblk
+#===========================================================
+#  Hblk is a POSIX-compliant system wide method of blocking
+# unwanted web traffic via host files.
+############################################################
+
+name="hblk"
+NAME="Hblk"
+version="0.0.1"
+
+source=""
