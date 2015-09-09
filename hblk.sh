9fbdf80488b86a3f789fd87363b9aed6d08b4f04
lemonlogic <jnthnlmn@gmail.com>
2015-09-09 18:36:53 -0400

    Update hblk.sh
    Changes to lists(), update(), and help.

--- hblk.sh
+++ hblk.sh
@@ -11,6 +11,7 @@ NAME="Hblk"
 version="0.0.1"
 
 
+
 # Curl or WGET?
 if [ -e "/bin/curl" ]; then
     echo "Found curl, so we're using curl."
@@ -26,10 +27,17 @@ fi
 
 
 lists() {
-    yoyo="http://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts"
-    mvps="http://winhelp2002.mvps.org/hosts.txt"
-
-    if
+    list1="http://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts"
+    list2="http://winhelp2002.mvps.org/hosts.txt"
+    list3=""
+    list4=""
+    if [ ! -e hres/lists.txt ]; then
+	cat > hres/lists.txt
+	echo "You have no sources for your blocklist."
+	echo "Added 2 default sources to blocklist."
+	echo $list1 >> hres/lists.txt
+	echo $list2 >> hres/lists.txt
+    fi
 }
 
 
@@ -37,15 +45,31 @@ lists() {
 case $1 in
     update)
 	echo "Starting download!" && sleep 3 && \
-	    $dler $yoyo >> ~/.hblk/yoyo && \
-	    $dler $mvps >> ~/.hblk/mvps && \
-	    echo "Download complete! Starting sort!" && sleep 3 && \
-	    sort -u ~/.hblk >> ~/.hblk && \
-	    echo "Sort complete!" || echo "It goof'd"
+	    if [ ! -e ~/.hblk ]; then
+		mkdir ~/.hblk
+	    fi
+
+	for i in lists
+	do
+	    $dler $list$i >> ~/.hblk/list$i.txt
+	done
+	echo "Download complete! Starting sort!" && sleep 3 && \
+        sort -u ~/.hblk >> ~/.hblk && \
+        echo "Sort complete!" || echo "It goof'd"
 	;;
 
     \?|-h|help|--help)
 
+	cat <<EOF
+
+Options
+---------------------------------------
+update,  -u   Update blocklist from source(s).
+block,   -B   Adds domain to local blocklist.
+unblock, -b   Removes domain from local blocklist.
+add,     -a   Adds a custom source for blocklist updates.
+remove,  -r   Removes a source from blocklist updates.
+EOF
 	;;
 
     *)
