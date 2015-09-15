fd95bb34eb30ef81440daa4859a66d5818d736ff
Johnathan C. Maudlin <jcmdln@users.noreply.github.com>
2015-09-14 23:26:50 -0400

    Update hblk.sh

--- hblk.sh
+++ hblk.sh
@@ -1,75 +1,75 @@
 #!/bin/sh
 ############################################################
-#  Hblk
+#    Hblk 
 #===========================================================
-#  Hblk is a POSIX-compliant system wide method of blocking
-# unwanted web traffic via host files.
+#
+#  
+#
+############################################################
+name="hblk"
+version="0.0.1-2"
+source="https://github.com/lemonlogic/hblk.git"
+depends=""
 ############################################################
 
-name="hblk"
-NAME="Hblk"
-version="0.0.1"
 
+help() {
+    cat <<EOF
+ Options
+---------------------------------------
+ update    Update blocklist from source(s).
 
+ add       Adds a custom source for blocklist updates.
 
-# Curl or WGET?
-if [ -e "/bin/curl" ]; then
-    echo "Found curl, so we're using curl."
-    dler="curl"
-else
-    if [ -e "/bin/wget" ]; then
-	echo "Found wget, so we'll use wget."
-	dler="wget"
-    else
-	echo "Install curl, wget, or edit this script and choose your own downloader!"
-    fi
-fi
+ remove    Removes a source from blocklist updates.
 
-
-lists() {
-    list1="http://pgl.yoyo.org/adservers/serverlist.php?showintro=0;hostformat=hosts"
-    list2="http://winhelp2002.mvps.org/hosts.txt"
-    list3=""
-    list4=""
-    if [ ! -e hres/lists.txt ]; then
-	cat > hres/lists.txt
-	echo "You have no sources for your blocklist."
-	echo "Added 2 default sources to blocklist."
-	echo $list1 >> hres/lists.txt
-	echo $list2 >> hres/lists.txt
-    fi
+EOF
 }
 
 
-# Execution
-case $1 in
-    update)
-	echo "Starting download!" && sleep 3 && \
-	    if [ ! -e ~/.hblk ]; then
-		mkdir ~/.hblk
-	    fi
+update() {
+    if [ -e "/bin/curl" ]; then
+	echo "Found curl, so we're using curl."
+	dler="curl"
+    else
+	if [ -e "/bin/wget" ]; then
+	    echo "Found wget, so we'll use wget."
+	    dler="wget"
+	else
+	    echo "Install curl, wget, or edit this script and choose your own downloader!"
+	fi
+    fi
 
-	for i in lists
-	do
-	    $dler $list$i >> ~/.hblk/list$i.txt
-	done
-	echo "Download complete! Starting sort!" && sleep 3 && \
-        sort -u ~/.hblk >> ~/.hblk && \
-        echo "Sort complete!" || echo "It goof'd"
-	;;
+    ## Blocklist(s)
+    yoyo="https://github.com/lemonlogic/hblk/raw/v0.0.1-2/lists/yoyo"
+    mvps="https:/github.com/lemonlogic/hblk/raw/v0.0.1-2/lists/mvps"
+    hphosts="http://hosts-file.net/download/hosts.txt"
+
+    #lists=yoyo,mvps,hphosts
+    #for $list in $lists; do
+    #
+    #done
+    
+    ## Create/Load blocklists
+    $dler $yoyo -o ~/.hblk
+    echo "Update complete!"
+}
+
+
+### Execution
+case $1 in
+    -U)
+        echo "Performing a HARD update"
+        #update_hard
+        ;;
+        
+    -u|update|--update)
+    	echo "Performing a safe update"
+    	update_safe
+    	;;
 
     \?|-h|help|--help)
-
-	cat <<EOF
-
-Options
----------------------------------------
-update,  -u   Update blocklist from source(s).
-block,   -B   Adds domain to local blocklist.
-unblock, -b   Removes domain from local blocklist.
-add,     -a   Adds a custom source for blocklist updates.
-remove,  -r   Removes a source from blocklist updates.
-EOF
+	help
 	;;
 
     *)
